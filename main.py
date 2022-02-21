f75ef5c5670dfc82db7bc0150d01deb5987e22ee
baron <baronchibuike@gmail.com>
2022-02-21 23:28:14 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+from turtle import up
+from urllib import response
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from model import Todo
+
+
+# app object
+app = FastAPI()
+
+from database import (
+    fetch_all_todos, 
+    fetch_one_todo, 
+    create_todo, 
+    update_todo, 
+    remove_todo)
+
+origins = ['hhtp://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+@app.get('/')
+def read_root():
+    return {"Ping": "Pong"}
+
+
+@app.get('/api/todo')
+async def get_todo():
+    response = await fetch_all_todos()
+    return response
+
+
+@app.get('/api/todo/{title}', response_model=Todo)
+async def get_todo_by_id(title: str):
+    response = await fetch_one_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f"there is no TODO item with this title - {title}")
+
+
+@app.post('/api/todo', response_model=Todo)
+async def post_todo(todo:Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, "something went wrong/ Bad request")
+
+
+@app.put('/api/todo/{title}', response_model=Todo)
+async def update_todo(title:str, description:str):
+    response = await update_todo(title, description)
+    if response:
+        return response
+    raise HTTPException(400, "something went wrong/ Bad request")
+
+
+@app.delete('/api/todo/{title}')
+async def delete_todo(title:str):
+    response = await remove_todo(title)
+    if response:
+        return "Successfully deleted todo items"
+    raise HTTPException(400, "something went wrong/ Bad request")
+    
